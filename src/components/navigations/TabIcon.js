7f66e8f93100a22c987bd9b8976fdfa8da7d8915
Cagatay <cagatay.kapuagasi@gmail.com>
2020-06-24 01:35:01 +0300

    colors

--- src/components/navigations/TabIcon.js
+++ src/components/navigations/TabIcon.js
@@ -4,7 +4,13 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class TabIcon extends Component {
   render() {
-    const color = this.props.focused ? colors.primary : 'white';
-    return <Icon name={this.props.iconName || 'user-o'} size={22} color={color} />;
+    const color = this.props.focused ? colors.primary : colors.lightGray;
+    return (
+      <Icon
+        name={this.props.iconName || 'user-o'}
+        size={22}
+        color={color}
+      />
+    );
   }
 }
