a706e9c8436ff2f7884f31a63d69fd8a6eda4d4c
Cagatay <cagatay.kapuagasi@gmail.com>
2020-06-24 01:34:55 +0300

    tabs

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,33 +1,61 @@
-import React, { useEffect } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+  View,
+  StyleSheet,
+  Text,
+  Dimensions,
+  ScrollView,
+  SafeAreaView,
+} from 'react-native';
 import { images, fonts, colors } from 'res';
 import { ScaledSheet } from 'react-native-size-matters';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
-import { useMutation } from '@apollo/react-hooks';
+import { useMutation, useQuery } from '@apollo/react-hooks';
 import { inject, observer } from 'mobx-react';
+import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
+import { getOrders, getRestaurants } from '~/utils/query';
+import Tab from './Tab';
 
-const signUp = gql`
-  mutation {
-    loginWithEmail(email: "oliverjones@gmail.com", password: "123456") {
-      token
-    }
-  }
-`;
+const { width } = Dimensions.get('window');
+
+const initialLayout = { width };
+
+const renderTabBar = props => (
+  <TabBar
+    {...props}
+    indicatorStyle={styles.indicatorStyle}
+    style={styles.tabStyle}
+    activeColor={colors.primary}
+    inactiveColor={colors.lightGray}
+  />
+);
+
+getDatas = ({ limit = 10, index = 0 }) => {};
+
+//{ loading, error, data }
 
 const Home = props => {
-  const [getToken, data] = useMutation(signUp);
-
-  useEffect(async () => {
-    console.log(await getToken());
-  }, []);
-
-  console.log('data', props);
+  const [index, setIndex] = useState(0);
+  const [routes] = useState([
+    { key: 'first', title: 'Orders' },
+    { key: 'second', title: 'Restaurants' },
+  ]);
+  const renderScene = SceneMap({
+    first: () => <Tab query={getOrders} tag="pastOrders" />,
+    second: () => <Tab query={getRestaurants} tag="restaurants" />,
+  });
 
   return (
-    <View style={styles.container}>
-      <Text>My Starter Kit</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <TabView
+        navigationState={{ index, routes }}
+        renderScene={renderScene}
+        renderTabBar={renderTabBar}
+        onIndexChange={setIndex}
+        initialLayout={initialLayout}
+      />
+    </SafeAreaView>
   );
 };
 
@@ -36,7 +64,11 @@ export default inject('store')(observer(Home));
 const styles = ScaledSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  },
+  tabStyle: {
+    backgroundColor: 'transparent',
+  },
+  indicatorStyle: {
+    backgroundColor: colors.primary,
   },
 });
