f627b581ecb494cb7b7daf3f57b69995d70939bf
Cagatay <cagatay.kapuagasi@gmail.com>
2020-06-23 23:00:48 +0300

    ignore yellowboxes

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry, YellowBox } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
+YellowBox.ignoreWarnings(['ReactNativeFiberHostComponent', 'Remote']);
 AppRegistry.registerComponent(appName, () => App);
